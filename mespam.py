29742a6db2e0688be3e9c72d06b5372286f2bdfe
Jonathan Franzone <jonathan@franzone.com>
2022-06-09 14:32:13 -0500

    Added YML configuration file and iterating through mailbox configurations

--- mespam.py
+++ mespam.py
@@ -1,12 +1,31 @@
+from ast import Import
 import sys
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+class MeSpamFilter:
+
+    def __init__(self):
+        with open('mailboxes.yml', 'r') as stream:
+            self.config = load(stream, Loader=Loader)['mailboxes']
+
+    def run(self, mailbox='ALL'):
+        print('Running MeSpamFilter against {0} mailbox(s)'.format(mailbox))
+        for mbox in self.config:
+            print(mbox, '->', self.config[mbox])
 
 def main() -> int:
     if len(sys.argv) >= 2:
-        name = sys.argv[1]
+        mailbox = sys.argv[1]
     else:
-        name = 'World'
+        mailbox = 'ALL'
 
-    print('Hello', name)
+    o = MeSpamFilter()
+    o.run(mailbox)
+
     return 0
 
 if __name__ == '__main__':
