1538910ac3a41d399e87bdbc0d8360640259ee9f
Jonathan Franzone <jonathan@franzone.com>
2022-06-13 11:24:51 -0500

    Stopped the script from marking messages as read. Printing out a few details about emails

--- mespam.py
+++ mespam.py
@@ -1,6 +1,10 @@
 from ast import Import
 import sys
 import imaplib
+import email
+import re
+import traceback
+import pprint
 from yaml import load
 try:
     from yaml import CLoader as Loader
@@ -25,12 +29,37 @@ class MeSpamFilter:
     def process_mailbox(self, mailbox):
         print('Processing mailbox for {0}'.format(mailbox['email']))
         mbox = self.open_inbox(mailbox)
+        counter = 0
         if mbox:
             try:
                 self.open_inbox(mailbox)
-                mbox.select()
+                mbox.select('INBOX.Filtered.SelfSpam')
+                typ, data = mbox.search(None, 'ALL')
+                print('Found {0} emails to process'.format(len(data[0].split())))
+                for num in data[0].split():
+                    try:
+                        # Get the message
+                        #typ, data = mbox.fetch(num, '(RFC822)')
+                        typ, data = mbox.fetch(num, '(BODY.PEEK[])')
+                        msg = email.message_from_string(data[0][1].decode('utf-8'))
+
+                        # Get JUST the email address and domain
+                        match = re.search(r'([\w\.-]+)(@[\w\.-]+)', msg['From'])
+                        emailAddr = match.group(0)
+                        emailDomain = match.group(2)
+
+                        #print(emailAddr)
+                        print('MSG ID: {0}, FROM: {1}, SUBJECT: {2}'.format(num, emailAddr, msg['Subject']))
+                        counter = counter + 1
+                        if counter >= 10:
+                            break
+                    except:
+                        print('Error processing email', sys.exc_info()[0])
+                        print(traceback.format_exc())
+                        break
             except:
                 print('Error processing inbox for {0}: {1}'.format(mailbox['email'], sys.exc_info()[0]))
+                print(traceback.format_exc())
             finally:
                 self.close_inbox(mbox)
 
